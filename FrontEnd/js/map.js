5b029644406f7b0b45add9afb3591a72f92ff972
GianlucaBortoli <giallogiallo93@gmail.com>
2014-05-17 10:20:01 +0200

    added example POI

--- FrontEnd/js/map.js
+++ FrontEnd/js/map.js
@@ -1,5 +1,4 @@
 map = new OpenLayers.Map("maps");
-map.addLayer(new OpenLayers.Layer.OSM());
 var mapnik = new OpenLayers.Layer.OSM();
 var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
 var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
@@ -9,6 +8,16 @@ var position = new OpenLayers.LonLat(latitude, longitude).transform(fromProjecti
 var zoom = 12; 
  
 map.addLayer(mapnik);
+
+var markerslonLat = new OpenLayers.LonLat(latitude, longitude).transform(//add lon/lat poi
+  new OpenLayers.Projection("EPSG:4326"), 
+  map.getProjectionObject() 
+);
+ 
+var markers = new OpenLayers.Layer.Markers("Markers");//create marker
+map.addLayer(markers);//add marker to level
+markers.addMarker(new OpenLayers.Marker(markerslonLat));
+
 map.setCenter(position, zoom);
 
 /*
