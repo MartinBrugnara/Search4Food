475b5a82c89d27741c2c9196f1f8394df46dae28
Martin Brugnara <martin.brugnara@gmail.com>
2014-05-20 14:42:32 +0200

    fix #020

---
+++ Merged/inc/header.inc.php
@@ -0,0 +1,13 @@
+<header>
+    <img src="img/logo.png" id="logo" alt="logo">
+    <input type="text" placeholder="Where do you want to eat?">
+    <input type="text" placeholder="What's your special purpose?">
+    <!-- TODO: bind event to search -->
+    <a><i class="fi-magnifying-glass">&nbsp;</i></a>
+
+    <div class="rw">
+        <i class="flag uk">&nbsp;</i>
+        <i class="flag it">&nbsp;</i>
+        <i class="fb">&nbsp;</i>
+    </div>
+</header>
