6e98a1015e0f96b869e4973b83fdd1074a5a4029
Fatbardha Hoxha <oxa.fat@gmail.com>
2014-05-23 22:35:34 +0200

    modifiche a profilo
    css quasi terminato

--- Merged/libs/db.php
+++ Merged/libs/db.php
@@ -5,7 +5,7 @@ $db_user = '';
 $db_pass = '';
 $db_name = '';
 
-mysql_connect($db_host, $db_user, $db_pass, $db_name) 
-    or die('[EE] DB connection error: '.mysqli_error($link));
+// mysql_connect($db_host, $db_user, $db_pass, $db_name) 
+//     or die('[EE] DB connection error: '.mysqli_error($link));
 
 ?>
