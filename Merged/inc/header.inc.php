2fe071b863ef3c3f8b8dd37df94ae461e0d64ef8
Alex De Biasio <alexdebiasio@gmail.com>
2014-05-23 11:25:30 +0200

    Work on #044

--- Merged/inc/header.inc.php
+++ Merged/inc/header.inc.php
@@ -1,4 +1,5 @@
     <header>
+
       <img src="/static/img/logo.png" id="logo" alt="logo">
 
       <?php if ($_SERVER['SCRIPT_NAME'] != '/index.php'): ?>
@@ -12,11 +13,17 @@
       <div class="options_wrapper">
         <a href="<?=$_SERVER['SCRIPT_NAME']?>?lang=eng" target="_self"><i class="flag en">&nbsp;</i></a>
         <a href="<?=$_SERVER['SCRIPT_NAME']?>?lang=ita" target="_self"><i class="flag it">&nbsp;</i></a>
-        <?php if(!$_GLOBALS['user']->is_logged): ?>
-        <i class="fb">&nbsp;</i>
+        <?php if(!($user_profile && isset($user_profile->identifier))): ?>
+        <a href="login-with.php?provider=Facebook"><i class="fb"></i></a>
         <?php else: ?>
-        <span class="user">Alex de biasio</span>
+        <span class="user">
+            <?php
+                echo $user_profile->displayName.'ciaociao';
+            ?>
+        </span>
         <a href="/logout.php" class="logout"><?=$GLOBALS['dict']->logout->{$_SESSION['lang']}; ?></a>
         <?php endif; ?>
       </div>
-    </header>
+
+
+   </header>
